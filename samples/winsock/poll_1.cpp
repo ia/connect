c6fcb5377a49099939bbd3dfc394d49711407154
Ivan Zorin <ivan.a.zorin@gmail.com>
2012-08-12 21:21:06 +0400

    libconnect: samples/: adding demos for multiplexing

---
+++ samples/winsock/poll_1.cpp
@@ -0,0 +1,163 @@
+
+#pragma comment (lib, "ws2_32.lib")
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <mstcpip.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define ERR(e) \
+	printf("%s:%s failed: %d [%s@%ld]\n",__FUNCTION__,e,WSAGetLastError(),__FILE__,__LINE__)
+
+#define CLOSESOCK(s) \
+	if(INVALID_SOCKET != s) {closesocket(s); s = INVALID_SOCKET;}
+
+#define DEFAULT_WAIT	30000
+
+#define WS_VER	  0x0202
+
+#define DEFAULT_PORT	12345
+
+#define TST_MSG	 "0123456789abcdefghijklmnopqrstuvwxyz\0"
+
+HANDLE hCloseSignal = NULL;
+
+int __cdecl main()
+{
+	WSADATA wsd;
+	INT nStartup = 0, nErr = 0, ret = 0;
+	SOCKET lsock = INVALID_SOCKET, asock = INVALID_SOCKET;
+	SOCKADDR_STORAGE    addr = {0};
+	WSAPOLLFD           fdarray = {0};
+	ULONG               uNonBlockingMode = 1;
+	CHAR                buf[MAX_PATH] = {0};
+	HANDLE              hThread = NULL;
+	DWORD               dwThreadId = 0;
+	
+	nErr = WSAStartup(WS_VER,&wsd);
+	if (nErr) {
+		WSASetLastError(nErr);
+		ERR("WSAStartup");
+		exit(0);
+	} else {
+		nStartup++;
+	}
+	
+	if (NULL == (hCloseSignal = CreateEvent(NULL, TRUE, FALSE, NULL))) {
+		ERR("CreateEvent");
+		exit(0);
+	}
+	
+	/*
+	if (NULL == (hThread = CreateThread(NULL, 0, ConnectThread, NULL, 0, &dwThreadId))) {
+		ERR("CreateThread");
+		//exit__leave;
+	}
+	*/
+	
+	addr.ss_family = AF_INET;
+	INETADDR_SETANY((SOCKADDR*)&addr);
+	SS_PORT((SOCKADDR*)&addr) = htons(DEFAULT_PORT);
+	
+	if (INVALID_SOCKET == (lsock = socket(AF_INET, SOCK_STREAM, 0))) {
+		ERR("socket");
+		exit(0);
+	}
+	
+	if (SOCKET_ERROR == ioctlsocket(lsock, FIONBIO, &uNonBlockingMode)) {
+		ERR("FIONBIO");
+		exit(0);
+	}
+	
+	if (SOCKET_ERROR == bind(lsock, (SOCKADDR*)&addr, sizeof (addr))) {
+		ERR("bind");
+		exit(0);
+	}
+	
+	if (SOCKET_ERROR == listen(lsock, 100)) {
+		ERR("listen");
+		exit(0);
+	}
+	
+	//Call WSAPoll for readability of listener (accepted)
+	
+	fdarray.fd = lsock;
+	fdarray.events = POLLRDNORM | POLLWRNORM;
+
+
+
+	if (SOCKET_ERROR == (ret = WSAPoll(&fdarray, 1, DEFAULT_WAIT))) {
+		ERR("WSAPoll");
+		exit(0);
+	}
+
+while (1) {
+	if (ret) {
+		if ((fdarray.revents & POLLRDNORM) || (fdarray.revents & POLLWRNORM)) {
+			printf("Main: Connection established.\n");
+			
+			if (INVALID_SOCKET == (asock = accept(lsock, NULL, NULL))) {
+				ERR("accept");
+				//__leave;
+			}
+			
+			/*
+			if (SOCKET_ERROR == (ret = recv(asock, buf, sizeof(buf), 0))) {
+				ERR("recv");
+			} else {
+				printf("Main: recvd %d bytes\n",ret);
+			}
+			*/
+		}
+	}
+	
+	//Call WSAPoll for writeability of accepted
+	
+	fdarray.fd = asock;
+	fdarray.events = POLLWRNORM;
+	
+	if (SOCKET_ERROR == (ret = WSAPoll(&fdarray, 1, DEFAULT_WAIT))) {
+		ERR("WSAPoll");
+		// exit
+	}
+	
+	if (ret) {
+		if (fdarray.revents & POLLWRNORM) {
+			if (SOCKET_ERROR == (ret = send(asock, TST_MSG, sizeof(TST_MSG), 0))) {
+				ERR("send");
+				// exit
+			} else {
+				printf("Main: sent %d bytes\n",ret);
+				closesocket(asock);
+			}
+		}
+	}
+
+}
+
+	//SetEvent(hCloseSignal);
+	
+	//WaitForSingleObject(hThread,DEFAULT_WAIT);
+	
+	/* clean up before exit */
+	
+	CloseHandle(hCloseSignal);
+	CloseHandle(hThread);
+	CLOSESOCK(asock);
+	CLOSESOCK(lsock);
+	
+	if (nStartup) {
+		WSACleanup();
+	}
+	
+	return 0;
+	
+}
+
---
+++ samples/winsock/signal.c
@@ -0,0 +1,61 @@
+
+#include <windows.h> 
+#include <stdio.h> 
+ 
+BOOL CtrlHandler( DWORD fdwCtrlType ) 
+{ 
+  switch( fdwCtrlType ) 
+  { 
+    // Handle the CTRL-C signal. 
+    case CTRL_C_EVENT: 
+      printf( "^C\n" );
+      printf( "EXIT\n" );
+      Beep( 750, 300 );
+      exit(0);
+      return( TRUE );
+ 
+    // CTRL-CLOSE: confirm that the user wants to exit. 
+    case CTRL_CLOSE_EVENT: 
+      Beep( 600, 200 ); 
+      printf( "Ctrl-Close event\n\n" );
+      return( TRUE ); 
+ 
+    // Pass other signals to the next handler. 
+    case CTRL_BREAK_EVENT: 
+      Beep( 900, 200 ); 
+      printf( "Ctrl-Break event\n\n" );
+      return FALSE; 
+ 
+    case CTRL_LOGOFF_EVENT: 
+      Beep( 1000, 200 ); 
+      printf( "Ctrl-Logoff event\n\n" );
+      return FALSE; 
+ 
+    case CTRL_SHUTDOWN_EVENT: 
+      Beep( 750, 500 ); 
+      printf( "Ctrl-Shutdown event\n\n" );
+      return FALSE; 
+ 
+    default: 
+      return FALSE; 
+  } 
+} 
+ 
+int main( void ) 
+{ 
+  if( SetConsoleCtrlHandler( (PHANDLER_ROUTINE) CtrlHandler, TRUE ) ) 
+  { 
+    printf( "\nThe Control Handler is installed.\n" ); 
+    printf( "\n -- Now try pressing Ctrl+C or Ctrl+Break, or" ); 
+    printf( "\n    try logging off or closing the console...\n" ); 
+    printf( "\n(...waiting in a loop for events...)\n\n" ); 
+ 
+    while( 1 ){;} 
+  } 
+  else 
+  {
+    printf( "\nERROR: Could not set control handler"); 
+    return 1;
+  }
+return 0;
+}
