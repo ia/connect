21c05d6d67d821f6dd9d82561ea60a69c71e2562
Ivan Zorin <ivan.a.zorin@gmail.com>
2013-01-26 13:31:09 +0400

    libconnect: lib/platform/platform.c: minor fixes in define directives

--- lib/platform/platform.c
+++ lib/platform/platform.c
@@ -3,9 +3,9 @@
 #	include "sys_linux.c"
 #	warning "PLATFORM: LINUX"
 #	ifndef SYS_LINUX
-#		define SYS_NT 1
+#		define SYS_LINUX 1
 #	endif
-#elif ( (!defined(__linux__)) && ((defined(__unix__)) || (defined(__APPLE__) && defined(__MACH__))) )
+#elif ( (!defined(__linux__)) && ( defined(__unix__) || ( defined(__APPLE__) && defined(__MACH__) ) || defined(__QNX__) ) )
 #	include "sys_bsd.c"
 #	warning "PLATFORM: BSD"
 #	ifndef SYS_BSD
